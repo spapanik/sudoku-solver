4439ba7cba76f76d8ea935c60de041e7c332aba5
Steph Papanik <spapanik21@gmail.com>
2019-06-15 12:51:51 +0100

    Add some helper functions

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -80,6 +80,43 @@ type get_element(unordered_set<type> non_empty_set) {
 }
 
 
+template <class type> unordered_set<type> intersection_set(unordered_set<type> first, unordered_set<type> second) {
+	unordered_set<type> output{};
+	for (type object: first) {
+		if (second.find(object) != second.end()) {
+			output.insert(object);
+		}
+	}
+
+	return output;
+}
+
+
+template <class type> unordered_set<type> union_set(unordered_set<type> first, unordered_set<type> second) {
+	unordered_set<type> output{};
+	for (type object: first) {
+		output.insert(object);
+	}
+	for (type object: second) {
+		output.insert(object);
+	}
+
+	return output;
+}
+
+
+template <class type> unordered_set<type> difference_set(unordered_set<type> first, unordered_set<type> second) {
+	unordered_set<type> output{};
+	for (type object: first) {
+		if (second.find(object) == second.end()) {
+			output.insert(object);
+		}
+	}
+
+	return output;
+}
+
+
 bool Sudoku::naked_single() {
 	bool advanced = false;
 	vector<int> deleted;
